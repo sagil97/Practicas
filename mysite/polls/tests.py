5a03218b8d7419aa67cbcca17af17d888d098240
Sara Andrea GIl Barros <sarandre@Mac-mini-de-Sara.local>
2020-07-21 11:29:11 +0200

    Parte 5

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.test import TestCase
 from django.utils import timezone
-from django.utils import reverse
+from django.urls import reverse
 
 from .models import Question
 
@@ -27,9 +27,9 @@ class QuestionModelTests(TestCase):
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
 
-    def create_question(question_text, days):
-        time = timezone.now() + datetime.timedelta(days=days)
-        return Question.objects.create(question_text=question_text, pub_date=time)
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
 
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
